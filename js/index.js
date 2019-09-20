f641fbf0f463d104db8b7d5d1aed7b28614f7da5
daw-catedra <55557179+daw-catedra@users.noreply.github.com>
2019-09-20 00:43:38 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,35 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    var user = firebase.auth().currentUser;
+    if (user != null) {
+      var email_id = user.email;
+      document.getElementById("user").innerHTML = "Bienvenido: " + email_id;
+    }
+  }
+});
+
+function login() {
+  var userEmail = document.getElementById("correo").value;
+  var userPass = document.getElementById("contra").value;
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(user => {
+    window.location = "main.html";
+  }).catch(error => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error : " + errorMessage);
+  })
+}
+
+function logout() {
+  firebase.auth().signOut();
+  window.location = "index.html";
+}
+
+function guardar() {
+  var ejemplo = document.getElementById("texto");
+  var basededatos = firebase.database().ref();
+  var ejemplo_text = ejemplo.value;
+  basededatos.child("EJEMPLO").set(ejemplo_text);
+
+}
+
